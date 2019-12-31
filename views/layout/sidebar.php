78e30ff1edd12b39782fdbb7298b61ce60f2eff0
Raul Gustavo <ragusmat@hotmail.com>
2019-12-31 14:12:48 -0300

    Registro de usuarios

--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,20 +1,32 @@
 <!-- BARRA LATERAL --> 
 <aside id="lateral">
     <div id="login" class="block_aside">
-        <h3>Entrar a la web</h3>
-        <form action="" method="post">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email"/>
-            <label for="password">Contraseña</label>
-            <input type="password" name="password" id="password"/>
-            <input type="submit" value="Enviar"/>
-        </form>
+        <?php if (!isset($_SESSION['identity'])): ?>
+            <h3>Entrar a la web</h3>
+            <form action="<?= base_url ?>usuario/login" method="post">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email"/>
+                <label for="password">Contraseña</label>
+                <input type="password" name="password" id="password"/>
+                <input type="submit" value="Enviar"/>
+            </form>
+        <?php else: ?>
+            <h3><?= $_SESSION['identity']->nombre ?>
+                <?= $_SESSION['identity']->apellidos ?> </h3>
+        <?php endif; ?>
 
         <ul>
-            <li><a href="">Mis pedidos</a></li>
-            <li><a href="">Gestionar pedidos</a></li>
-            <li><a href="">Gestionar categorías</a></li>
-        </ul>
+       
+            <?php if (isset($_SESSION['admin'])): ?>
+                <li><a href="#">Gestionar categorías</a></li>
+                <li><a href="#">Gestionar productos</a></li>
+                <li><a href="#">Gestionar pedidos</a></li>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['identity'])): ?>  
+                <li><a href="#">Mis pedidos</a></li>
+                <li><a href="<?= base_url ?>usuario/logout">Cerrar sesión</a></li>
+            <?php endif; ?>
     </div>
 </aside>
 
--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -1,6 +1,14 @@
 <h1>Registrarse</h1>
 
-<form action="index.php?controller=usuario&action=save" method="POST">
+<?php
+if(isset($_SESSION['register']) && $_SESSION['register'] == 'complete'): ?>
+<strong class="alert_green">Registro completado correctamente</strong>
+<?php elseif(isset($_SESSION['register']) && $_SESSION['register'] == 'failed'): ?>
+<strong class="alert_red">Registro fallido, introduce bien los datos</strong>
+<?php endif; ?>
+<?php Utils::deleteSession('register'); ?>
+
+<form action="<?=base_url?>usuario/save" method="POST">
     <label for="nombre">Nombre</label>
     <input type="text" name="nombre" id="nombre" required>
     
