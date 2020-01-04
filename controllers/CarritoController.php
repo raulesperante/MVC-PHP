86a76de725e8acf242386b638730aafde0bff832
Raul Gustavo <ragusmat@hotmail.com>
2020-01-04 17:21:35 -0300

    Carrito de la compra

---
+++ controllers/CarritoController.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once 'models/producto.php';
+
+class CarritoController {
+
+    public function index() {
+        echo 'Contralor Carrito, Accion Index';
+        var_dump($_SESSION['carrito']);
+    }
+
+    public function add() {
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+        } else {
+            header('Location:' . base_url . 'carrito/index');
+        }
+
+        if (isset($_SESSION['carrito'])) {
+            $counter = 0;
+            foreach ($_SESSION['carrito'] as $indice => $elemento) {
+                if ($elemento['id_producto'] == $id) {
+                    $_SESSION['carrito'][$indice]['unidades'] ++;
+                    $counter++;
+                }
+            }
+        }
+        if (!isset($counter) || $counter == 0) {
+            // Consegur producto
+            $producto = new Producto();
+            $producto->setId($id);
+            $producto = $producto->getOne();
+
+            // Añadir al carrito
+            if (is_object($producto)) {
+                $_SESSION['carrito'][] = array(
+                    'id_producto' => $producto->id,
+                    'precio' => $producto->precio,
+                    'unidades' => 1,
+                    'producto' => $producto
+                );
+            }
+        }
+        header('Location:' . base_url . 'carrito/index');
+    }
+
+    public function remove() {
+        
+    }
+
+    public function delete_all() {
+        unset($_SESSION['carrito']);
+        header('Location:' . base_url . 'carrito/index');
+    }
+
+}
--- views/producto/ver.php
+++ views/producto/ver.php
@@ -11,7 +11,7 @@
         <div class="data">
             <p class="descripcion"><?= $pro->descripcion ?></p>
             <p class="price">$ <?= $pro->precio ?></p>
-            <a href="" class="button">Comprar</a>
+            <a href="<?=base_url?>carrito/add&id=<?=$pro->id?>" class="button">Comprar</a>
         </div>
     </div>
 <?php else: ?>
